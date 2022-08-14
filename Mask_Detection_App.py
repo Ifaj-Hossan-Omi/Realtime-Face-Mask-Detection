4acccb57fe0282f89e3a9d3a704de2af7e11fd83
Ifaj Hossan Omi <20-42387-1@student.aiub.edu>
2022-08-14 13:03:49 +0600

    Final Commit

--- Mask_Detection_App.py
+++ Mask_Detection_App.py
@@ -1,14 +1,14 @@
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from os.path import dirname, join
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-from imutils.video import VideoStream
-import numpy as np
-import imutils
-import time
+# import all packages
 import cv2
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from imutils.video import VideoStream
+from tensorflow.keras.models import load_model
+from os.path import dirname, join
+import imutils
+import numpy as np
 import os
-
+import time
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
     (h, w) = frame.shape[:2]
@@ -23,7 +23,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
     locs = []
     preds = []
 
-    # loop over the detections
+
     for i in range(0, detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         if confidence > 0.5:
@@ -33,34 +33,31 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
             (startX, startY) = (max(0, startX), max(0, startY))
             (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-            # convert it from BGR to RGB channel and ordering, resize
             face = frame[startY:endY, startX:endX]
             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
             face = cv2.resize(face, (224, 224))
             face = img_to_array(face)
             face = preprocess_input(face)
 
-            # bounding boxes to their respective lists
             faces.append(face)
             locs.append((startX, startY, endX, endY))
 
-    # only make a predictions if at least one face was detected
     if len(faces) > 0:
         faces = np.array(faces, dtype="float32")
         preds = maskNet.predict(faces, batch_size=32)
     return (locs, preds)
 
 
-# load our serialized face detector model from disk
+# load our serialized face detector model from loacal machine
 prototxtPath = r"deploy.protext"
 weightsPath = r"res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
-maskNet = load_model("mask_detector.model")
+# load the face mask detector model from loacal machine
+maskNet = load_model("Mask_Detector_Model.model")
 
-# initialize the video stream
-print("Starting the CAMERA...")
+# initialize camera
+print("CAMERA is starting >>")
 vs = VideoStream(src=0).start()
 
 # loop over the frames from the video stream
@@ -90,10 +87,9 @@ while True:
     cv2.imshow("Mask Detection App", frame)
     key = cv2.waitKey(1) & 0xFF
 
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
+    # if the `x` key was pressed, break from the loop
+    if key == ord("x"):
         break
 
-# do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
